74e79f2e07a545de18dd35c23cca0b176603baed
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2022-06-20 16:09:44 +0300

    second attempt (summer life)

--- src/components/Buttons/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import s from './Button.modul.css'
-
-export const Button = (props) => {
-    return (
-        <button className={s.button} onClick={props.callback}>{props.name}</button>
-    )
-}
